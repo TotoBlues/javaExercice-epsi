124e936cd7554f36a8a47296f6f72a61ac2bc76b
cedric clemenceau <cedric.clemenceau@epitech.eu>
2017-11-15 08:20:18 +0100

    architecture code

---
+++ b3-javaProjet_1/src/Csv.java
@@ -0,0 +1,10 @@
+import java.io.File;
+
+public class Csv {
+	private int fd;
+	
+	public Csv(String file) {
+		File csvFile;
+		
+	}
+}
---
+++ b3-javaProjet_1/src/Etiquettes.java
@@ -0,0 +1,4 @@
+
+public class Etiquettes extends FicheSortie {
+
+}
